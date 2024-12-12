817bcd0547002d39350af2f638229e07d79e6e52
Wojciech Malarski <w.malarski@gmail.com>
2024-12-12 20:46:11 +0100

    updates preset

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
 	server: {
-		preset: "cloudflare-pages-static",
+		preset: "cloudflare-pages",
 		rollupConfig: {
 			external: ["node:async_hooks"],
 		},
